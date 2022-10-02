aa78a37094b35af86556c4fb8218a9dd34c523f9
Sayan Chowdhury <98570396+sayan011@users.noreply.github.com>
2022-10-02 17:59:23 +0530

    Update deploy.ts

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -5,10 +5,9 @@ async function main() {
   const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
   const unlockTime = currentTimestampInSeconds + ONE_YEAR_IN_SECS;
 
+  
   const lockedAmount = ethers.utils.parseEther("1");
 
-  
-  
   
   const Lock = await ethers.getContractFactory("Lock");
   const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
